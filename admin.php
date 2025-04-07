38be0af2bcd950ebfb74c5196ff9b8f28e224d9e
Pierre Laforest <laforestpierre1@gmail.com>
2025-04-07 11:04:47 +0200

    ajout de commentaire sur le fichier admin.php

--- admin.php
+++ admin.php
@@ -3,6 +3,7 @@
     <head>
         <title>Park d'attraction</title>
         <meta charset="utf-8">
+        <!--lien qui relie la page admin.php au css-->
         <link rel="stylesheet" type="text/css" href="accueil.css" />
         <!-- lien pour le logo de page-->
    	    <link rel="icon" href="logo.jpeg">
@@ -10,20 +11,21 @@
     <body>
       <!-- Inclusion du fichier hautPage.php pour afficher le menu -->
       <?php
-      include "hautPage.php";
+      include "hautPage.php"; // inclusion de la page haut de page 
       ?>
       <br>
       <!-- Titre de présentation -->
       <div class="z1">
         <p class="textepresentation">Page Admin</p>
         <br>
+        <!-- Bouton qui permet de se déconnecter de la session -->
         <button type="submit" class="btnDeconnexion" onclick="deconnexion()">Déconnexion</button>
       </div>
       <br>
       
      <!-- Inclusion du fichier basPage.php pour afficher le bas de page -->
      <?php
-     include "basPage.php";
+     include "basPage.php"; // inclusion de la page bas de page
      ?>
     </body>
 </html>
