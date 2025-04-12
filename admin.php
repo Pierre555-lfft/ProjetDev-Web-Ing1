095b08d09b0eff89024cb7ffdbf8542252eeebe4
Pierre Laforest <laforestpierre1@gmail.com>
2025-04-12 15:04:48 +0200

    ajout d'une condition pour obliger l'utilisation de passer par le portail de connexion

--- admin.php
+++ admin.php
@@ -1,9 +1,18 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user']) || $_SESSION['user'] !== 'admin') {
+    header('Location: connexion.php');
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>Park d'attraction</title>
         <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="accueil.css" />
+        <link rel="stylesheet" type="text/css" href="admin.css" />
         <!-- lien pour le logo de page-->
    	    <link rel="icon" href="logo.jpeg">
     </head>
@@ -15,9 +24,7 @@
       <br>
       <!-- Titre de présentation -->
       <div class="z1">
-        <p class="textepresentation">Page Admin</p>
-        <br>
-        <button type="submit" class="btnDeconnexion" onclick="deconnexion()">Déconnexion</button>
+        <p class="textepresentation">Bonjour <?php echo isset($_SESSION['login']) ? $_SESSION['login'] : 'Administrateur'; ?></p>
       </div>
       <br>
       
